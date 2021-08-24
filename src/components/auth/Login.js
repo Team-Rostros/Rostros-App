a5ef49b937773d5e6ad8f594d0b0a05049a9d5af
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-08-24 18:53:18 -0500

    Retornando a la validacion original

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,14 +1,8 @@
-// Predefined packages
-import React, {useState, useContext} from 'react';
+import React, {Fragment, useState, useContext} from 'react';
 import { Link } from 'react-router-dom';
-
-// Custom packages
 import AlertaContext from '../../context/alertas/alertaContext';
-import { useForm } from '../../hooks/useForm';
-
-// Assets
+//Assets
 import People from '../../img/people_search.png';
-import { regExpEmail, regExpPassword } from '../../utils/validator';
 
 const Login = () => {
 
@@ -16,42 +10,25 @@ const Login = () => {
     const alertaContext = useContext(AlertaContext);
     const {alerta, mostrarAlerta} = alertaContext;
 
-    // Custom hook form estados inicio sesión
+    //State para iniciar sesión
 
-    const [values, handleInputChange] = useForm({
-        email: '',
+    const [usuario, guardarUsuario] = useState({
+        email:'',
         password: ''
     });
 
-    // Extracting Errors
-    const [error, setError] = useState({
-        eEmail: "0",
-        ePassword: "0"
-    });
-
-    const { eEmail, ePassword } = error;
-
-    // Validar campos
-    const validateAndShow = (e) => {
-
-        const { target } = e;
-        const { name } = target;
-
-        switch (name) {
-            case 'email':
-                setError({ ...error, eEmail: handleInputChange(e, regExpEmail) });
-                break;
-            case 'password':
-                setError({ ...error, ePassword: handleInputChange(e, regExpPassword) });
-                break;
-            default:
-                break;
-        }
-    }
-
     //Extrayendo el usuario
 
-    const {email, password} = values;
+    const {email, password} = usuario;
+
+    
+
+    const onChange = (e)=>{
+        guardarUsuario({
+            ...usuario,
+            [e.target.name]:[e.target.value]
+        })
+    }
 
     //Cuando el usuario inicie sesion
 
@@ -70,7 +47,7 @@ const Login = () => {
 
     }
     return (
-        <>
+        <Fragment>
             <div className="layout__grid layout__grid--login">
                 <div className="grid__sidebar grid__sidebar--login">
                     <div className="contenedor">
@@ -98,9 +75,7 @@ const Login = () => {
                                     id="email"
                                     name="email"
                                     value={email}
-                                    onChange={validateAndShow}
-                                />
-                                {!eEmail && <p className="error">Campo inválido</p>}
+                                    onChange={onChange} />
                             </div>
 
                             <div className="input">
@@ -111,28 +86,28 @@ const Login = () => {
                                     id="password"
                                     name="password"
                                     value={password}
-                                    onChange={validateAndShow}
-                                />
-                                {!ePassword && <p className="error">Campo inválido</p>}
+                                    onChange={onChange} />
                             </div>
 
 
                             
-                            <Link className="enlace" to="/rpassword">¿Olvidaste tu contraseña?</Link>
+                            <a className="enlace" href="rpassword.html">¿Olvidaste tu contraseña?</a>
                             <input 
                                 type="submit"
                                 value="Iniciar Sesión"
                                 className="boton boton--primario centrar-bloque"
                             />
-                            <Link to='/nueva-cuenta' className="boton boton--secundario centrar-bloque">
+                            <Link to={'/nueva-cuenta'} className="boton boton--secundario centrar-bloque">
                                 Registrarse
                             </Link>
+                            
+                            
                         </form>
                     </div>
                 </div>
                 
             </div>
-        </>
+        </Fragment>
     );
 }
  
--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -75,6 +75,24 @@ const AuthState = props => {
         }
     }
 
+    //Cuando el usuario inicia sesion
+    const iniciarSesion = async datos => {
+        try{
+            const respuesta = await clienteAxios.post('/api/auth', datos);
+            console.log(respuesta);
+        }catch(error){
+             //console.log(error.response.data.msg);
+             const alerta = {
+                msg: error.response.data.msg,
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: LOGIN_ERROR,
+                payload: alerta
+            })
+        }
+    }
+
 
     return(
         <AuthContext.Provider
@@ -83,7 +101,8 @@ const AuthState = props => {
                 autenticado: state.autenticado,
                 usuario: state.usuario,
                 mensaje: state.mensaje,
-                registrarUsuario
+                registrarUsuario,
+                iniciarSesion
             }}
         >{props.children}</AuthContext.Provider>
     )
