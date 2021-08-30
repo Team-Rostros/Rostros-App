89d5158777a78721ae03e3c2cedd88fe2d061e82
stiven4950 <omarrocha4950@gmail.com>
2021-08-30 10:07:39 -0500

    Corregido requestApi

--- src/utils/requestApi.js
+++ src/utils/requestApi.js
@@ -1,15 +1,17 @@
-import { API } from "../config";
 import clienteAxios from "../config/axios";
 
-export const getOptions=async(pais, query)=> {
+export const getOptions = async (pais, query) => {
 
-        const {data} = await clienteAxios.get(`${API}/ciudad/${pais}/${query===''?'A':query}`);
-        
-        return data.map(({ _id, nombre }) => ({ value: _id, name: nombre }));
+        if (pais !== '') {
+
+                const { data } = await clienteAxios.get(`/ciudad/${pais}/${query === '' ? 'A' : query}`);
+                return data.map(({ _id, nombre }) => ({ value: _id, name: nombre }));
+        }
+        return [];
 }
 
-export const getPaises = async()=>{
+export const getPaises = async () => {
 
-        const {data} = await clienteAxios.get(`${API}/pais/list`);
+        const { data } = await clienteAxios.get(`/pais/list`);
         return data;
 }
